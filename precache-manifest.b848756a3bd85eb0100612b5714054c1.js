bc0f232e13c0add26eca8fd0128121860d5b9ce1
naveen-naik02 <naveennaik@piktorlabs.com>
2022-01-06 09:59:45 +0530

    Updates

---
+++ precache-manifest.b848756a3bd85eb0100612b5714054c1.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "d657bcbeddf018cbedc2",
+    "url": "/naveen-naik02/Assignment-4.git/static/css/main.18cd5379.chunk.css"
+  },
+  {
+    "revision": "d657bcbeddf018cbedc2",
+    "url": "/naveen-naik02/Assignment-4.git/static/js/main.d657bcbe.chunk.js"
+  },
+  {
+    "revision": "08611444a190981da534",
+    "url": "/naveen-naik02/Assignment-4.git/static/js/1.08611444.chunk.js"
+  },
+  {
+    "revision": "cd62348bda50734da3ac",
+    "url": "/naveen-naik02/Assignment-4.git/static/js/2.cd62348b.chunk.js"
+  },
+  {
+    "revision": "91624270f0a642f6ad4b",
+    "url": "/naveen-naik02/Assignment-4.git/static/js/runtime~main.91624270.js"
+  },
+  {
+    "revision": "b942868885eb6bf98a6a2b1072762632",
+    "url": "/naveen-naik02/Assignment-4.git/static/media/safe_icon.b9428688.png"
+  },
+  {
+    "revision": "8202df7419b03e953308034bc8e4c278",
+    "url": "/naveen-naik02/Assignment-4.git/static/media/add.8202df74.png"
+  },
+  {
+    "revision": "dbad6eed443e7278b41c3229bb49e9aa",
+    "url": "/naveen-naik02/Assignment-4.git/static/media/logo.dbad6eed.svg"
+  },
+  {
+    "revision": "bf2790d72f5f433759f842f6e6b15abf",
+    "url": "/naveen-naik02/Assignment-4.git/static/media/secret.bf2790d7.png"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/naveen-naik02/Assignment-4.git/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "065e2862567e17d0f1a094c35d3a9067",
+    "url": "/naveen-naik02/Assignment-4.git/static/media/safe.065e2862.png"
+  },
+  {
+    "revision": "f2508677131d8f407cdbb21b6d732295",
+    "url": "/naveen-naik02/Assignment-4.git/static/media/safe-banner.f2508677.png"
+  },
+  {
+    "revision": "b90e31fbfe2e2a8b8da685cd360c2a41",
+    "url": "/naveen-naik02/Assignment-4.git/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/naveen-naik02/Assignment-4.git/precache-manifest.b848756a3bd85eb0100612b5714054c1.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/naveen-naik02/Assignment-4.git/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
